4dae9d0ac962c8ca26fbb7a30eade3570f6b1908
Maryse <maryselt@umich.edu>
2018-06-21 12:07:26 -0400

    part 3

--- part1.py
+++ part1.py
@@ -4,7 +4,7 @@ import nltk
 import json
 import sys
 
-
+import secrets
 
 # /////////////////////////////////////////
 # ////////// README Instructions //////////
@@ -14,15 +14,17 @@ import sys
     # FILENAME: part1.py
 
     # ARGUMENTS: the program takes two arguments--a twitter username and the number of tweets to analyze.
+        #sys.argv[1]
+        #sys.argv[2]
 
     # OUTPUT: the program outputs the following:
 
-        # the user name
-        # the number of tweets analyzed
+        # the user name  == ##'screen_name'
+        # the number of tweets analyzed  == ##'count'
         # the five most frequent verbs that appear in the analyzed tweets
         # the five most frequent nouns that appear in the analyzed tweets
         # the five most frequent adjectives that appear in the analyzed tweets
-        # the number of original tweets (i.e., not retweets)
+        # the number of original tweets (i.e., not retweets) == ##'include_rts'
         # the number of times that the original tweets in the analyzed set were favorited
         # the number of times that the original tweets in the analyzed set were retweeted by others
         # To determine the five most frequent, ties should be broken alphabetically, capitals before lowercase. (Check out Python stable sorting for a relatively easy way to handle this.)
@@ -32,7 +34,8 @@ import sys
     # NOTES:
 
         # use tweepy for accessing the Twitter API
-        # Use NLTK for analyzing parts of speech. Use NLTK's default POS tagger and tagset (this will use the UPenn treebank tagset)--you do NOT need to install any taggers or tagsets into NLTK.
+        # Use NLTK for analyzing parts of speech.
+            # Use NLTK's default POS tagger and tagset (this will use the UPenn treebank tagset)--you do NOT need to install any taggers or tagsets into NLTK.
         # "stop words": ignore any words that do not start with an alphabetic character [a-zA-Z], and also ignore 'http', 'https', and 'RT' (these show up a lot in Twitter)
         # a "verb" is anything that is tagged VB*
         # a "noun" is anything that is tagged NN*
@@ -71,3 +74,169 @@ import sys
 
 # write your code here
 # usage should be python3 part1.py <username> <num_tweets>
+
+username = sys.argv[1]
+num_tweets = sys.argv[2]
+
+# ////////////////////////////////////////////////
+# ////////// STEP 1 - Setting Up Access //////////
+# ////////////////////////////////////////////////
+
+consumer_key = secrets.CONSUMER_KEY
+consumer_secret = secrets.CONSUMER_SECRET
+access_token = secrets.ACCESS_KEY
+access_secret = secrets.ACCESS_SECRET
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+user_tweets = api.user_timeline(screen_name=username, count=num_tweets)
+
+
+# /////////////////////////////////////////////////////
+# ////////// STEP 2 - Sorting Through Tweets //////////
+# /////////////////////////////////////////////////////
+the_tweets = []
+for tweet in user_tweets:
+    the_tweets.append(tweet.text.lower())
+
+    #print(tweet.text) ## Non list format (just text)
+    #print(the_tweets) ## List format (inside [])
+#print(type(the_tweets))
+
+
+
+
+# //////////////////////////////////////////////////////
+# ////////// STEP 3 - Sorting Through Cache ///////////
+# /////////////////////////////////////////////////////
+
+
+#text_res = []
+#for abc in the_tweets:
+#    text_res.append(abc.get('text'))
+
+text_str = "".join(str(x) for x in the_tweets) ## Now it's a string
+#print(type(text_str))
+tokenizer = nltk.word_tokenize(text_str) ## List of words, not letters
+#print(type(tokenizer))
+dictt = nltk.FreqDist(tokenizer)
+
+
+# ///////////////////////////////////////////
+# ////////// Removing "stop words" //////////
+# ///////////////////////////////////////////
+
+real_words = []
+for z in tokenizer:
+    if z.isalpha():
+        real_words.append(z)
+    #else:
+        continue
+
+    word_counter = nltk.FreqDist(real_words)
+    #dictt = nltk.pos_tag(tokenizer)
+sorted_dict = sorted(word_counter.items(), key= lambda x: x[1], reverse = True) # List of tuples
+
+#print(sorted_dict)
+
+# //////////////////////////////////////////////////////
+# ////////// Deleting Abbrevs from Words LIST //////////
+# //////////////////////////////////////////////////////
+
+stop_words = ['RT','http','https']
+for stopper in list(real_words):
+    if stopper in stop_words:
+        real_words.remove(stopper)
+
+
+    word_counter = nltk.FreqDist(real_words)
+#print(word_counter)
+
+tup_list = nltk.pos_tag(real_words) ## My LIST of unsorted word types
+
+nouns = []
+
+for word,pos in tup_list:
+    if pos == 'NN':
+        nouns.append(word)
+print(nouns)
+
+noun_count = []
+for word,freq in sorted_dict:
+    if word in nouns:
+        #noun_count.append(word)
+
+#print(noun_count)
+
+
+
+# /////////////////////////////////////////
+# ////////// Most Frequent Words //////////
+# /////////////////////////////////////////
+
+sorted_list = sorted(tup_list, key = lambda x: x[1], reverse = False)
+#print(sorted_list)
+
+
+# sorted_dict = sorted(dictt.items(), key= lambda x: x[1], reverse = True)
+# final_sort = sorted_dict[:5]
+
+#print("USER: {}\nTWEETS ANALYZED: {}\nThe 5 Most Frequent Words: {}\n".format(username, num_tweets, final_sort))
+
+# ////////////////////////////////////
+# ////////// Types of Words //////////
+# ////////////////////////////////////
+
+
+#for word,variations in sorted(word_type):
+#    print(word,variations)
+
+        # a "verb" is anything that is tagged VB*
+        # a "noun" is anything that is tagged NN*
+        # an "adjective" is anything that is tagged JJ*
+
+
+
+
+# ////////////////////////////////////////////////////
+# ////////// STEP 4 - Printing the Results ///////////
+# ////////////////////////////////////////////////////
+
+#print("USER: " + username)
+#print("TWEETS ANALYZED" + num_tweets)
+#print("VERBS: {}{()}, {}{()}, {}{()}, {}{()}, {}{()}".format(?))
+#print("NOUNS: {}{()}, {}{()}, {}{()}, {}{()}, {}{()}".format(?))
+#print("ADJECTIVES: {}{()}, {}{()}, {}{()}, {}{()}, {}{()}".format(?))
+#print("ORIGINAL TWEETS: {}".format(?))
+#print("TIMES FAVORITED (ORIGINAL TWEETS ONLY): {}".format(?))
+#print("TIMES RETWEETED (ORIGINAL TWEETS ONLY): {}".format(?))
+
+
+# ///////////////////////////////////////////////////////
+# ////////// STEP 5 - Saving Results in a CSV ///////////
+# ///////////////////////////////////////////////////////
+
+
+    # SAMPLE CSV FILE OUTPUT inside a .csv file
+
+        # Noun,Number
+        # umsi,8
+        # UMSI,8
+        # students,5
+        # Join,5
+        # amp,5
+
+#tweet_csv = open('twitter_results.csv', 'w', newline='')
+#file_writer = csv.writer(tweet_csv)
+#for tweet in #<analyzed list variable>:
+#    file_writer.writerow(Noun,Number)
+#    file_writer.writerow(username,num_tweets)
+#    file_writer.writerow(<var>,<count>)
+#tweet_csv.close()
+
+# //////////////////////////////////////////////////////////
+# ////////// Part 2 - Simple Database Application //////////
+# //////////////////////////////////////////////////////////
--- part3.py
+++ part3.py
@@ -13,22 +13,6 @@ from bs4 import BeautifulSoup
 
     # In part 3, you will use Beautiful Soup to scrape the Most Read stories from the Michigan Daily site (http://michigandaily.com), and will crawl one level deeper to find the author of each Most Read story.
 
-    # SAMPLE OUTPUT:
-
-        # dhcp3-213:507waiver2 mwnewman$ python3 part3.py
-
-        # Michigan Daily -- MOST READ
-        # Darkness and the Occult: A brief history of doom metal
-        #   by Selena Aguilera
-        # "The white rice was excellent. Followed the directions on the bag perfectly. Way to go."
-        #   by Hunter Zhao
-        # Schlissel: "I try not to have a personal opinion" on potential C.C. Little renaming, awaiting further review
-        #   by Alexa St. John
-        # A guide to Michigan's 2018 gubernatorial race
-        #   by Colin Beresford
-
-    # NOTES: If there is more than one reporter, you only need to print the first name on the byline.
-
 
 # ////////////////////////////////////////////
 # /////////////// Start Coding ///////////////
@@ -36,3 +20,91 @@ from bs4 import BeautifulSoup
 
 # write your code here
 # usage should be python3 part3.py
+
+# ////////////////////////////////////////////
+# ////////// Scraping the Home Page //////////
+# ////////////////////////////////////////////
+
+baseurl = 'https://www.michigandaily.com'
+html = requests.get('https://www.michigandaily.com/').text
+soup = BeautifulSoup(html, 'html.parser')
+#print(soup.prettify())
+
+searching_div = soup.find(id='mini-panel-right_sidebar_standard')
+#print(searching_div)
+
+most_read = searching_div.find_all('li') # Most Read Headlines & Urls
+
+headlines = []
+for h in most_read:
+    headlines.append(h.text)
+#    #print(h.text) # Just Headline Text
+
+most_read_urls = []
+for y in most_read:
+    most_read_urls.append(baseurl + y.a['href']) # Url to each headline
+
+#print(most_read_urls)
+
+# ////////////////////////////////////////////////
+# ////////// Searching Individual Pages //////////
+# ////////////////////////////////////////////////
+
+headline_authors = {} # Healines: Authors Dict; Keep Above Loop
+author_list = [] # Keep Above loop
+
+for abc in most_read_urls:
+    url = '{}'.format(abc)
+    #print(url)
+    each_story = requests.get(url).text # Making Each Page Request
+    story_soup = BeautifulSoup(each_story, 'html.parser')
+
+
+    # ///////////////////////////////////
+    # ////////// Headline Text //////////
+    # ///////////////////////////////////
+
+    #headline_section = story_soup.find(class_='tmdleft') # Overall
+
+    #headline_text = headline_section.find('h2') # Specific Section
+    ##print(headline_text)
+
+    #if len(headline_text) == 0: # In case of old HTML
+    #    headline = "The Most Read headline html format has changed dramatically over the years, and in this case my scraping code can't find it.\n"
+
+    #else:
+    #    element in headline_text: # If current HTML
+        ##headline = element.text
+    #    print(element)
+
+    ##print(headline)
+
+
+    # /////////////////////////////////
+    # ////////// Author Text //////////
+    # /////////////////////////////////
+
+
+    author_section = story_soup.find(class_='byline')
+
+    if author_section is not None: # If current HTML
+        author_text = author_section.find('a')
+        author = author_text.text
+        author_list.append(author)
+
+    else:
+        if author_section is None: # If old HTML
+            author = "The Michigan Daily HTML format has changed dramatically over the years, and in this case my scraping code can't find an author."
+            author_list.append(author)
+
+
+    #print(author)
+
+for x in range(len(headlines)):
+    headline_authors[headlines[x]] = author_list[x]
+
+print("Michigan Daily -- MOST READ\n")
+
+for x,y in headline_authors.items():
+   print(x)
+   print("     by " + y +"\n")
